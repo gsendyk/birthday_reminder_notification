dd5cfdd16ac9b3ac3516e96acfaf68d3cbb12f96
Guilherme Sendyk <31812417+gsendyk@users.noreply.github.com>
2022-07-04 20:17:13 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # birthday_reminder_notification
-Reminds you of upcoming birthdays based on your Google Contacts list.
+Reminds you of upcoming birthdays based on your Google Contacts list. It sends a pre-formatted message to your Telegram chat so you can just click on it and pre-fill over WhatsApp
+
+## Requirements
+
+1. Python 3.7+
+
+2. Create Google OAuth2 [credentials](https://developers.google.com/identity/protocols/oauth2) using the `https://www.googleapis.com/auth/contacts.readonly` scope, and save the `credentials.json` file into the root of this project.
+
+3. Telegram bot (create yours via [BotFather](https://t.me/botfather)). More info [here](https://core.telegram.org/bots#3-how-do-i-create-a-bot), and update the `config.yaml` file with your bot API key as well as your chat_id.
+
+## Usage
+
+1. Install the required libraries on a virtual enviroment (e.g. python3.7 -m pip install -r requirements.txt)
+
+2. Run `grab_contacts.py` (e.g. python3.7 grab_contacts.py). This will save your google contacts to a csv file (bday_detials.csv)
+
+3. Set up a cron job to run `message.py` once a day. This file will check for existing birthdays on the day of, and if it detects any, will send you a message over Telegram. 
---
+++ grab_contacts.py
@@ -0,0 +1,122 @@
+import csv
+import logging
+import os.path
+from collections import defaultdict
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+ME = "Replace Me as in full name matching your google account (e.g. John Doe)"
+SCOPES = ["https://www.googleapis.com/auth/contacts.readonly"]
+
+
+def login_google():
+    """
+    Logs into Google and save credentials
+    """
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
+
+
+def retrive_contacts(creds):
+    """
+    Will retrieve up to 1k contacts names, phone numbers and birthdays
+    https://developers.google.com/people/api/rest/v1/people.connections/list
+    """
+    try:
+        service = build("people", "v1", credentials=creds)
+        results = (
+            service.people()
+            .connections()
+            .list(
+                resourceName="people/me",
+                pageSize=1000,
+                personFields="names,phoneNumbers,birthdays",
+            )
+            .execute()
+        )
+        connections = results.get("connections", [])
+        return connections
+    except HttpError as err:
+        logging.exception(err)
+        return False
+
+
+def prep_dict(contacts_list):
+    """
+    This will prepare a dict list to be able to save on a csv file
+    """
+
+    try:
+        data = defaultdict(list)
+        for person in contacts_list:
+            birthday = person.get("birthdays", [])
+            if birthday:
+                if person["names"][0]["displayName"].title() != ME:
+                    date = person["birthdays"][0]["date"]
+                    month = f"{date['month']:02}"
+                    day = f"{date['day']:02}"
+                    number = [
+                        number["canonicalForm"].replace("+", "")
+                        for number in person["phoneNumbers"]
+                        if number["type"] == "mobile"
+                    ]
+
+                    data["displayName"].append(person["names"][0]["displayName"].title())
+                    data["givenName"].append(person["names"][0]["givenName"].title())
+                    data["phoneNumber"].append(number)
+                    data["birthday"].append(f"{month}/{day}")
+
+        return data
+
+    except Exception as e:
+        logging.exception(e)
+        return False
+
+
+def main():
+
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
+    logging.info("Starting...")
+
+    logging.info("Trying to log into Google...")
+    login = login_google()
+
+    logging.info("Trying to retrieve contacts...")
+    contacts_list = retrive_contacts(login)
+
+    if contacts_list:
+        prep_data = prep_dict(contacts_list)
+
+        if prep_data:
+            logging.info(f"Trying to save output to a csv file...")
+            final_data = zip(*prep_data.values())
+            with open("bday_details.csv", "w", newline="") as f:
+                csv_output = csv.writer(f)
+                csv_output.writerow(list(prep_data.keys()))
+                csv_output.writerows(final_data)
+
+            f.close()
+            logging.info(f"Saved")
+
+    logging.info(f"Exiting...")
+
+
+if __name__ == "__main__":
+    main()
